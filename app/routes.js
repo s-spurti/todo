763adbe28092443969dce4178cfe834fdeae5823
spurti <spurti.shetty@raweng.com>
2016-11-16 10:54:13 +0530

    app/routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,32 +3,32 @@ var ToDo = require ('./models/todo');
 module.exports = function(app){
 
 	//to display all the todos
-	app.get('/api/todo', function(req, res){
+	app.get('/api/todos', function(req, res){
 		ToDo.find(function(err,todos){
 			if (err)
-				res.send(err)
-				res.json(todos);
+   			return res.send(err)
+   			res.json(todos);
 		});
 	});
 		//to create a new todo
-	app.post('/api/todo', function(req, res){
+	app.post('/api/todos', function(req, res){
 
 		ToDo.create({
 			text: req.body.text,
 			done:false
-		}, function(err, todos){
+		},function(err, todos){
 			 if (err)
 			 	res.send(err);
 			 ToDo.find(function(err, todos){
 			 	if (err)
-			 		res.send(err)
-			 		res.json(todos);	
+   				return res.send(err)
+   				res.json(todos);
 			 });	
 		});
 	});
 
 	//to delete a todo
-		app.delete('/api/todo/:todoId', function(req, res){
+		app.delete('/api/todos/:todoId', function(req, res){
 			ToDo.remove({
 				_id : req.params.todoId;
 			}, function (err, todos){
@@ -36,8 +36,8 @@ module.exports = function(app){
 					res.send(err);
 				ToDo.find(function(err,todos){
 					if (err)
-						res.send(err)
-						res.json(todos);
+   					return res.send(err)
+   					res.json(todos);
 				});
 			});
 		});
